e21a85fee7a75801003383795450405602ecc26a
Souqna Marketplace <souqnamarketplace@gmail.com>
2026-04-28 01:09:50 +0000

    File changes

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 import { useSearchParams, Link } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
